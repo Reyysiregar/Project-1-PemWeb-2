7f2366be00fb35a76f0fba9ad296784d4cd6017d
Reyysiregar <deniyusuf008@gmail.com>
2024-05-08 14:21:44 +0700

    Projek 1

---
+++ dashboard.php
@@ -0,0 +1,242 @@
+<?php
+//include_once('top.php');
+//include_once('menu.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Puskesmas Padang Sidempuan</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+
+  <!-- Vendor CSS Files -->
+  <link href="Landing/Lumia/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="Landing/Lumia/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="Landing/Lumia/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="Landing/Lumia/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="Landing/Lumia/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="Landing/Lumia/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Lumia
+  * Template URL: https://bootstrapmade.com/lumia-bootstrap-business-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+      <div class="logo me-auto">
+        <h1><a href="index.html">UPTD-MEDAN</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="dashboard.php">Home</a></li>
+          <li><a class="nav-link scrollto" href="pasien.php">Pasien</a></li>
+          <li><a class="nav-link scrollto" href="paramedik.php">Paramedik</a></li>
+          <li><a class="nav-link scrollto " href="periksa.php">Periksa</a></li>
+          <li><a class="nav-link scrollto" href="unit_kerja.php">Layanan puskesmas</a></li>
+          <!--<li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>-->
+          <li><a class="nav-link scrollto" href="login.php">Login</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links d-flex align-items-center">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="container text-center text-md-left" data-aos="fade-up">
+      <h1>Selamat Datang di<span> UPTD-MEDAN</span></h1>
+      <h2>Kami bersedia melayanin anda dalam 24 jam kedepan.</h2>
+      <a href="#about" class="btn-get-started scrollto">Cek sekarang</a>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= What We Do Section ======= -->
+    <section id="what-we-do" class="what-we-do">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Tentang</h2>
+          <p>Mengenai puskesmas padang sidempuan bisa cek disini semua</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box">
+              <div class="icon"><i class='bx bx-plus-medical' style='color:#3ba6fa'></i></div>
+              <h4><a href="">Tenaga Medis</a></h4>
+              <p>Di tangani oleh tenaga medis yang berpengalaman</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4><a href="">Fasilitas</a></h4>
+              <p>Fasilitas yang cukup baik untuk menunjang kesehatan pasien</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4><a href="">24 Jam</a></h4>
+              <p>Pelayanan 24 Jam extra dalam melayani pasien</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End What We Do Section -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="Landing/Lumia/assets/img/uptd.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <h3>Sejarah UPTD-MEDAN</h3>
+            <p>
+             UPTD-MEDAN berdiri sejak tahun 2001 yang sudah banyak melayani pasien sebanyak 1.500 pasien, dan menjadi puskesmas satu-satunya
+             yang menjadi puskesmas kepercayaan warga Padang Sidempuan
+            </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 footer-contact">
+        <h3>UPTD-MEDAN</h3>
+        <p>
+         Medan <br>
+          Padang Sidempuan Jl.Kejayaan No.72<br><br>
+          <strong>Phone:</strong> +62 5589 55488 55<br>
+          <strong>Email:</strong>Uptd_Medan@gmail.com<br>
+        </p>
+      </div>
+
+      <div class="col-lg-2 col-md-6 footer-links">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+        </ul>
+      </div>
+
+     
+     
+
+    </div>
+  </div>
+</div>
+
+<div class="container d-md-flex py-4">
+
+  <div class="me-md-auto text-center text-md-start">
+    <div class="copyright">
+      &copy; Copyright <strong><span>UPTD-MEDAN</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/lumia-bootstrap-business-template/ -->
+      Designed by <a href="https://bootstrapmade.com/">Raihand Sirregar</a>
+    </div>
+  </div>
+  <div class="social-links text-center text-md-right pt-3 pt-md-0">
+    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+  </div>
+</div>
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="Landing/Lumia/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="Landing/Lumia/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="Landing/Lumia/assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="Landing/Lumia/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="Landing/Lumia/assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="Landing/Lumia/assets/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="Landing/Lumia/assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="Landing/Lumia/assets/js/main.js"></script>
+
+</body>
+<?php
+//include_once('bottom.php');
+?>
---
+++ proses_login.php
@@ -0,0 +1,20 @@
+<?php
+include_once('koneksi.php');
+
+//menangkap data yang dikirim dari from login
+$email = $_POST['email'];
+$password = md5($_POST['password']);
+
+//bua query untuk menangkap data user berdasarkan email dan password
+$query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+$result = $dbh->query($query)->fetch();
+
+if($result) {
+    session_start();
+    $_SESSION['email'] = $email;
+    $_SESSION['nama'] = $result['nama'];
+    header('location:dashboard.php');
+}
+
+
+?>
